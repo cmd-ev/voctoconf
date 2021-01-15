f18c44062539aa2a52f54623272773e9f2b7c32c
Guido Schmitz <g.schmitz@gtrs.de>
2021-01-15 12:41:14 +0100

    redirect landingpage immediately if page is live

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from partners.models import Partner
 from django.conf import settings
 
@@ -9,4 +9,6 @@ def index(request):
     context['venueless_partners'] = Partner.objects.filter(hide=False, bbb__isnull=True).order_by("order") # lul :3
     context['page_live'] = settings.PAGE_LIVE or (request.GET.get("preview") is not None)
 
+    if settings.PAGE_LIVE:
+        return redirect('eventoverview')
     return render(request, "index.html", context)
